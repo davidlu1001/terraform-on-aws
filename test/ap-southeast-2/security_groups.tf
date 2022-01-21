5424d7269c3668bc73110b77f3b8dea4f075167a
David Lu <david@adroitcreations.com>
2022-01-21 22:42:01 +1300

    Create security groups

---
+++ test/ap-southeast-2/security_groups.tf
@@ -0,0 +1,96 @@
+# Accepts all traffic from entire VPC range for routing
+resource "aws_security_group" "nat" {
+  name        = "nat"
+  description = "Used for NAT Gateways (managed)"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["${var.subnet_octets}.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_security_group" "internal-icmp" {
+  name        = "internal-icmp"
+  description = "Useful internal ICMP (managed)"
+
+  ingress {
+    protocol    = "icmp"
+    from_port   = 8
+    to_port     = -1
+    cidr_blocks = ["${var.subnet_octets}.0.0/16"]
+  }
+
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_security_group" "bastion" {
+  name        = "bastion"
+  description = "Bastion and SSH gateway rules (managed)"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+
+  vpc_id = module.vpc.vpc_id
+}
+
+# Accepts traffic on tcp:22 from bastion nodes for management
+resource "aws_security_group" "ssh" {
+  name        = "ssh"
+  description = "SSH access from the gateway (managed)"
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+
+  vpc_id = module.vpc.vpc_id
+}

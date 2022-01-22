370c6d398f8e005d5b04b56a1584492456c84137
David Lu <david@adroitcreations.com>
2022-01-23 00:43:19 +1300

    LB security groups

--- test/ap-southeast-2/security_groups.tf
+++ test/ap-southeast-2/security_groups.tf
@@ -94,3 +94,38 @@ resource "aws_security_group" "ssh" {
 
   vpc_id = module.vpc.vpc_id
 }
+
+# ALB Security Group (Traffic Internet -> ALB)
+resource "aws_security_group" "alb" {
+  name        = "load_balancer_security_group"
+  description = "Controls access to the ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

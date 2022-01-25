bf69119410f7509ffc235300ce0863655233e80d
David Lu <david@adroitcreations.com>
2022-01-25 17:32:19 +1300

    Add ami for ecs

--- test/ap-southeast-2/ami.tf
+++ test/ap-southeast-2/ami.tf
@@ -7,3 +7,13 @@ data "aws_ami" "amazon_linux" {
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
+
+data "aws_ami" "aws_optimized_ecs" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
+  }
+}

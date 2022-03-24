c924a41dfb5f4c97ba96c8beeba1801ce6ec18e1
David Lu <david@adroitcreations.com>
2022-03-24 20:05:05 +1300

    Temp disable remote backend for Prod env

--- prod/ap-southeast-2/state.tf
+++ prod/ap-southeast-2/state.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "state-bucket-app-prod"
-    key            = "ap-southeast-2/prod"
-    region         = "ap-southeast-2"
-    encrypt        = true
-    dynamodb_table = "state-lock-app-prod"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "state-bucket-app-prod"
+#    key            = "ap-southeast-2/prod"
+#    region         = "ap-southeast-2"
+#    encrypt        = true
+#    dynamodb_table = "state-lock-app-prod"
+#  }
+#}
